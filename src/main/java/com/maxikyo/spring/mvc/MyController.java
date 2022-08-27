780b46289d0c0f18d87d708a6a9a65b53d37f2ce
maxikyo <holovanovmax@gmail.com>
2022-08-27 19:50:37 +0200

    create Controller

--- src/main/java/com/maxikyo/spring/mvc/MyController.java
+++ src/main/java/com/maxikyo/spring/mvc/MyController.java
@@ -1,9 +1,13 @@
 package com.maxikyo.spring.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MyController {
 
-
+    @RequestMapping("/")
+    public String showFirstView(){
+        return "first-view";
+    }
 }
