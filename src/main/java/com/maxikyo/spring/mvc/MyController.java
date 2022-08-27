084443313b249fa4d50c5dc4b03ff71be9d3a8c7
maxikyo <holovanovmax@gmail.com>
2022-08-27 19:46:15 +0200

    MVC start (create Controller)

---
+++ src/main/java/com/maxikyo/spring/mvc/MyController.java
@@ -0,0 +1,9 @@
+package com.maxikyo.spring.mvc;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+
+}
---
+++ src/main/webapp/WEB-INF/view/first-view.jsp
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+
+<body>
+
+<h2>I wish you luck in Spring MVC learning</h2>
+
+</body>
+
+
+</html>
