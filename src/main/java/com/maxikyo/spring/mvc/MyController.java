1e723bdd919e3eb9722015a4f249a10031790095
maxikyo <holovanovmax@gmail.com>
2022-08-27 21:03:51 +0200

    change method (l-6)

--- src/main/java/com/maxikyo/spring/mvc/MyController.java
+++ src/main/java/com/maxikyo/spring/mvc/MyController.java
@@ -23,16 +23,16 @@ public class MyController {
 //    public String showEmpDetails(){
 //        return "show-emp-details-view";
 //    }
-    @RequestMapping("/showDetails")
-    public String showEmpDetails(HttpServletRequest request, Model model){
-
-        String empName = request.getParameter("employeeName");
-        empName = "Mr. " + empName;
-        model.addAttribute("nameAttribute", empName);
-
-        model.addAttribute("description", " - udemy instructor");
-
-
-        return "show-emp-details-view";
-    }
+//    @RequestMapping("/showDetails")
+//    public String showEmpDetails(HttpServletRequest request, Model model){
+//
+//        String empName = request.getParameter("employeeName");
+//        empName = "Mr. " + empName;
+//        model.addAttribute("nameAttribute", empName);
+//
+//        model.addAttribute("description", " - udemy instructor");
+//
+//
+//        return "show-emp-details-view";
+//    }
 }
