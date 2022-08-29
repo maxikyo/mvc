3f1ca9d57c147accf933c06379758c2af2046741
maxikyo <holovanovmax@gmail.com>
2022-08-29 19:16:24 +0200

    update and finished own annotation

--- src/main/java/com/maxikyo/spring/mvc/validation/CheckEmailValidator.java
+++ src/main/java/com/maxikyo/spring/mvc/validation/CheckEmailValidator.java
@@ -2,14 +2,20 @@ package com.maxikyo.spring.mvc.validation;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
-
+//дженерики<...> 1 - анотация, 2 - тип данных
 public class CheckEmailValidator implements ConstraintValidator<CheckEmail, String> {
+    private String endOfEmail;
+
     @Override
-    public void initialize(CheckEmail constraintAnnotation) {
+    public void initialize(CheckEmail checkEmail) {
+        endOfEmail = checkEmail.value();
     }
 
     @Override
-    public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
-        return false;
+    //business logic!!!
+    public boolean isValid(String enteredValue
+            , ConstraintValidatorContext constraintValidatorContext) {
+        //tru если соответсвует xyz.com/false если нет
+        return enteredValue.endsWith(endOfEmail);
     }
 }
