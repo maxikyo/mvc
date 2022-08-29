963796f8f8fabaa250fd8332b22801c04f79c801
maxikyo <holovanovmax@gmail.com>
2022-08-29 18:28:47 +0200

    l13 (Min/Max)

--- src/main/java/com/maxikyo/spring/mvc/Employee.java
+++ src/main/java/com/maxikyo/spring/mvc/Employee.java
@@ -1,9 +1,6 @@
 package com.maxikyo.spring.mvc;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,6 +12,8 @@ public class Employee {
 //    @NotEmpty(message = "surname is required field")
     @NotBlank(message = "surname is required field")
     private String surname;
+    @Min(value = 500, message = "must be greater than 499")
+    @Max(value = 1000, message = "must be less than 1001")
 
     private int salary;
 
